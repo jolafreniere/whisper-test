191f008dd49b2b052efb54eb52e24bd4f3638f9d
jlafreniere <jonathan.lafreniere@gmail.com>
2023-09-07 10:54:01 -0400

    move command parsing logic into recorder for now, refactor

--- recorder.js
+++ recorder.js
@@ -25,26 +25,29 @@ const recordAudio = async () => {
 };
 
 const main = async () => {
-  try {
-    let exit = false;
+  await listen();
+};
+
+async function listen() {
+  while (true) {
+    await recordAudio();
+    console.log("chunk recorded, attempting transcription");
+    let txt = await transcribe();
+    parseCommand(txt);
     try {
       await fs.unlink("./audio.wav");
-    } catch (error) {}
-    while (!exit) {
-      await recordAudio();
-      console.log("chunk recorded, attempting transcription");
-      exit = await transcribe().then(async () => {
-        try {
-          await fs.unlink("./audio.wav");
-        } catch (error) {
-          console.log("Error deleting audio.wav:", error);
-        }
-      });
+    } catch (error) {
+      console.log("Error deleting audio.wav:", error);
     }
-    console.log("EXIT CALLED, Exiting program");
-  } catch (error) {
-    console.log("An error occurred:", error);
   }
-};
+}
+
+async function parseCommand(baseString) {
+  let cleanValue = baseString.text.toLowerCase().trim().replace(".", "");
+  if (cleanValue.indexOf("gptexit") > -1) {
+    console.log("EXIT COMMAND RECEIVED, Shutting down");
+    process.exit();
+  }
+}
 
 main();
